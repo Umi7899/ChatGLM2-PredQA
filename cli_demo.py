1968dfff803774c12d9f6b1969f36f3ce1e74ae1
ZRH <543135875@qq.com>
2023-09-23 16:06:27 +0800

    none

--- cli_demo.py
+++ cli_demo.py
@@ -5,8 +5,8 @@ from transformers import AutoTokenizer, AutoModel
 import readline
 import lottery_predicter_pl_end
 
-tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True)
-model = AutoModel.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True).cuda()
+tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b-int4", trust_remote_code=True)
+model = AutoModel.from_pretrained("THUDM/chatglm2-6b-int4", trust_remote_code=True).cuda()
 # 多显卡支持，使用下面两行代替上面一行，将num_gpus改为你实际的显卡数量
 # from utils import load_model_on_gpus
 # model = load_model_on_gpus("THUDM/chatglm2-6b", num_gpus=2)
